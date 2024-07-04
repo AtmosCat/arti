de16f546502be358da74bf17769c0f60aa297937
hojeong_jeong <dnflro22@gmail.com>
2024-07-04 10:55:18 +0900

    MyPage 코드 새 파일로 옮기고 나서 첫 커밋

--- app/src/main/java/com/example/arti/MainActivity.kt
+++ app/src/main/java/com/example/arti/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        println("Hello")
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/arti/UserInfo.kt
+++ app/src/main/java/com/example/arti/UserInfo.kt
@@ -1,6 +1,6 @@
 package com.example.article
 
-class UserInfo(nickname: String, startupField: String, password: String, passwordCheck: String) {
+class UserInfo(nickname: String, startupField: String, password: String, passwordCheck: String ) {
     var nickname: String
     var startupField: String
     var password: String
