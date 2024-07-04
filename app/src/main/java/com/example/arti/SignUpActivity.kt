fbfd72ed01205977eebeeca33611492958db362f
hanhyelim <vldjtldb@naver.com>
2024-07-04 17:51:28 +0900

    레이아웃 기본

--- app/src/main/java/com/example/arti/SignUpActivity.kt
+++ app/src/main/java/com/example/arti/SignUpActivity.kt
@@ -1,6 +1,7 @@
 package com.example.arti
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +16,9 @@ class SignUpActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
+
         }
     }
 }
