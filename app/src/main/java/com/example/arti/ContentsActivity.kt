102134405c72e0acb2cdf789deea81a548f7084f
wavideo <kt8218@gmail.com>
2024-07-05 10:18:57 +0900

    끝! 목표한 모든 기능 100% 완료. 하단레이아웃 on/off 구현까지 완료.

--- app/src/main/java/com/example/arti/ContentsActivity.kt
+++ app/src/main/java/com/example/arti/ContentsActivity.kt
@@ -1,25 +1,32 @@
 package com.example.arti
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet.Motion
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 
 class ContentsActivity : AppCompatActivity() {
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_contents)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.anywher_touch)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.anywhere_touch)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
@@ -71,119 +78,24 @@ class ContentsActivity : AppCompatActivity() {
         btn_detail.setOnClickListener(){
             contents = ContentsManager.myContents[index].contents
             et_contents.setText(contents)
-            btn_detail.visibility = View.INVISIBLE
-
-
+            btn_detail.isVisible = false
         }
+        //더보기 버튼 -> 클릭하면 :: 전체내용을 보여주고, 버튼을 숨깁니다
 
+        val layout_bottom = findViewById<RelativeLayout>(R.id.layout_bottom)
+        layout_bottom.isVisible = true  // 하단버튼 on/off
 
-        // 화면 아무 곳이나 터치하면 on/off 되게 하고 싶었으나,
-//        콘스트레인트 레이아웃 (화면 전체)나
-//                android.R.id.content를 <View> 사용해도 안되길래
-//        일단 포기!
-
-//        val anywhere_touch = findViewById<ConstraintLayout>(R.id.anywher_touch)
-//        anywhere_touch.setOnClickListener() {
-//            finish()
-//        }
-//
-//        val anywhere_touch = findViewById<View>(android.R.id.content)
-//        anywhere_touch.setOnClickListener() {
-//            finish()
-//        }
-
-//        anywhere_touch.setOnTouchListener { v, event ->
-//            if (event.action == MotionEvent.ACTION_DOWN) {
-//             finish()
-//            }
-//            true
-//        }
+        val anywhere_touch = findViewById<LinearLayout>(R.id.linear_layout)
+        anywhere_touch.setOnTouchListener { v, event -> // "리니어 레이아웃"에 대한 터치리스너
+            if (btn_detail.isVisible == false && event.action == MotionEvent.ACTION_UP) {
+                    layout_bottom.isVisible = !layout_bottom.isVisible
+            }
+            true
+        }
+        // [ 터치리스너 - 하단레이아웃 ON/OFF ]
+        /* 손을 떼면, 하단레이아웃 on/off 전환
+        불리언을 토글할 때는 a = !a 로 뒤집을 수 있다 */
 
     }
 
 }
-
-
-//어떤 기능을 구현해야할까?
-
-/** 기능개발0. 정보를 받아와서 게시글 보여주는 기능
- - ContentsClass의 인스턴스 Id가 몇번 글인지 받아온다.
-001번 글이면 001번 인스턴스의 모든 정보를 찾아줘!
- 001. 이미지
- 001. 제목
- 001. 추천인
-
- 001. 추천이유
- 001. 본문
- 001. 링크
-
- - 본문을 125자로 줄인다.
- - 더보기 버튼을 visible
-
- */
-
-/** 기능개발1.빈칸을 클릭하면 하단 레이아웃을 visible
-아무데나 클릭하면,
-하단 레이아웃 (더보기버튼, 원문링크 버튼, 그라디언트) visible
-다시 아무데나 클릭하면 invisible 된다
-
-단, 더보기를 누르기 전에는
-invisible 할수 없다
- */
-
-
-/** 기능개발2. 원문 링크로 이동하는 기능
- - 클릭리스너 {
- 암시적 intent -> 인터넷 브라우저 : "인스턴스. 링크 "
- }
- */
-
-/** 기능개발3. 더보기 버튼
-
- - 클릭 리스너 {
-본문 내용을 125자 -> 무제한 풀어주는 기능
-더보기 버튼 invisible
- }
- */
-
-/** 기능개발4. 뒤로가기 버튼
-- 클릭리스너 {
-intent -> main 액티비티
-or
-Cancel
-}
- */
-
-
-
-
-/**
-btn_back
-뒤로가기 버튼
-
-img_title
-타이틀 이미지
-
-et_editor
-추천인
-
-et_title
-제목 (1-2줄, 25자)
-
-et_reason
-추천이유 (1줄, 20자)
-
-et_contents
-본문내용
-(더보기 전 : 125자)
-
-layout_bottom
-하단버튼 가리개
-
-btn_detail
-더보기 버튼
-
-btn_outlink
-원본 링크 이동하기 버튼
- */
-
